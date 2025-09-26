0bc453fe3bbc8bc3d4890f095a346a6a8c9ff6ee
Andreas Post <privat@andreaspost.com>
2025-09-26 15:13:18 +0200

    Better formatting for map marker popups.

--- angular-maps-frontend/src/app/service/map-data.service.ts
+++ angular-maps-frontend/src/app/service/map-data.service.ts
@@ -9,6 +9,10 @@ import { PointOfInterest } from "../model/point_of_interest";
 })
 export class MapDataService {
 
+    private iconPhone = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+</svg>`;
+
     /**
      * Creates the popup content for a given point of interest.
      * @param poi Point of interest
@@ -108,7 +112,17 @@ export class MapDataService {
         var details = poi.details;
 
         if (details) {
-            details = details.replaceAll('\n', '<br>');
+            // split with regex matching either ", " or "\n"
+            var detailChunks = details.split(/, |\n/);
+
+            detailChunks.forEach((chunk, index) => {
+                chunk = this.formatForLink(chunk);
+                chunk = this.formatForPhone(chunk);
+
+                detailChunks[index] = chunk;
+            });
+
+            details = detailChunks.join('<br>');
         }
 
         iconImg += `<br>${details}`;
@@ -136,4 +150,36 @@ export class MapDataService {
         }
         return 1000;
     }
+
+    /**
+     * Formats a text string for use as a link if the string starts with http or www.
+     * @param text Text to format
+     * @returns Formatted text
+     */
+    formatForLink(text: string): string {
+        let urlFormat = text.trim();
+        if (urlFormat.startsWith("http") || urlFormat.startsWith("www.")) {
+            urlFormat = `<a href="${urlFormat}" target="_blank" rel="noopener">Link</a>`;
+        }
+        return urlFormat;
+    }
+
+    /**
+     * Add a phone icon for text that starts with Tel.: or +49. So not the best recognition, but works for now.
+     * @param text Text to format
+     * @returns Formatted text
+     */
+    formatForPhone(text: string): string {
+        let phoneFormat = text.trim();
+
+        if (phoneFormat.startsWith("+49")) {
+            phoneFormat = this.iconPhone + ' ' + phoneFormat;
+        }
+
+        if (phoneFormat.startsWith("Tel.:")) {
+            phoneFormat = phoneFormat.replace("Tel.:", this.iconPhone);
+        }
+        return phoneFormat;
+    }
+
 }
