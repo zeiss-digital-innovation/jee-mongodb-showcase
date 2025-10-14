d39892943f9db4f34a565dea00e0c50187369b2d
Andreas Post <privat@andreaspost.com>
2025-10-14 12:56:17 +0200

    Fixed unit tests.

--- angular-maps-frontend/src/app/point-of-interest-map/point-of-interest-map.component.spec.ts
+++ angular-maps-frontend/src/app/point-of-interest-map/point-of-interest-map.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PointOfInterestMapComponent } from './point-of-interest-map.component';
@@ -8,16 +9,29 @@ describe('PointOfInterestMapComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PointOfInterestMapComponent]
+      imports: [PointOfInterestMapComponent, HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(PointOfInterestMapComponent);
     component = fixture.componentInstance;
+
+    // create a dummy map container so Leaflet initialization in ngOnInit doesn't fail
+    const mapEl = document.createElement('div');
+    mapEl.id = 'map';
+    mapEl.style.width = '600px';
+    mapEl.style.height = '400px';
+    document.body.appendChild(mapEl);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    const el = document.getElementById('map');
+    if (el && el.parentNode) el.parentNode.removeChild(el);
   });
 });
