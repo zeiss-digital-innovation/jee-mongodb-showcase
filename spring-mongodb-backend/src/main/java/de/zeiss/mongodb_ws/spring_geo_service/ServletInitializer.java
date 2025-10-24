469f379f1cf13d8198782f0d617ed656d70ce1b6
Andreas Post <privat@andreaspost.com>
2025-10-24 15:09:12 +0200

    Initial commit for spring boot based backend. App starts with same context-root and CORS configuration as the other backends. REST operations already in place, but currently only working with mocked data (or even without implementation).

---
+++ spring-mongodb-backend/src/main/java/de/zeiss/mongodb_ws/spring_geo_service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package de.zeiss.mongodb_ws.spring_geo_service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringGeoServiceApplication.class);
+	}
+
+}
---
+++ spring-mongodb-backend/src/test/java/de/zeiss/mongodb_ws/spring_geo_service/SpringGeoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.zeiss.mongodb_ws.spring_geo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGeoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
